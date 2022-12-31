af97626d78a96ab46b927512299514d7a5a67e79
David K. Kim <rlaruddlf711@naver.com>
2022-12-31 17:20:29 +0900

    Add code to serve static assets

--- mern/server/server.js
+++ mern/server/server.js
@@ -17,3 +17,11 @@ app.listen(port, () => {
     });
     console.log(`Server is running on port: ${port}`);
 });
+
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
